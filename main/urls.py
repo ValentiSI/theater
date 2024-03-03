d3b217b97cd75e521411514b0f76a4bccd9bdac4
ValentiSI <tinkamv@gmail.com>
2024-03-03 17:31:04 +0300

    feat: add application main

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('time/', views.time_view, name='time'),
+    path('script/', views.script_view, name='script'),
+]
---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def home_view(request: HttpResponse):
+    return HttpResponse(f'''
+        <h1 style='color: blue;'>Hello, {request.user.email}!</h1>
+    ''')
+    
+def time_view(request: HttpResponse):
+    return HttpResponse(f'<h3>{datetime.now().strftime("%A")}  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</h3>')
+
+def script_view(request: HttpResponse):
+    return HttpResponse(f'''
+        <script>
+            alert('Hello, {request.user.email}!');
+        </script> <a href="http://127.0.0.1:8000/time/">Time now</a>
+    ''')
