1ff2b839f2efb501b3c40008705033ea3c971b58
ValentiSI <tinkamv@gmail.com>
2024-03-11 21:39:13 +0300

    creating a ticket model and a workdjango.txt file, add img in static

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='home'),
-    path('time/', views.time_view, name='time'),
-    path('script/', views.script_view, name='script'),
 ]
--- main/views.py
+++ main/views.py
@@ -8,15 +8,4 @@ from datetime import datetime
 #     ''')
 
 def index_view(request: HttpResponse):
-    return HttpResponse(render(request, 'products.html', {}))
-
-   
-def time_view(request: HttpResponse):
-    return HttpResponse(f'<h3>{datetime.now().strftime("%A")}  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</h3>')
-
-def script_view(request: HttpResponse):
-    return HttpResponse(f'''
-        <script>
-            alert('Hello, {request.user.email}!');
-        </script> <a href="http://127.0.0.1:8000/time/">Time now</a>
-    ''')
+    return HttpResponse(render(request, 'index.html', {}))
