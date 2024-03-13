b33a185512386bad8f90291ebedfe1f2c82141d8
ValentiSI <tinkamv@gmail.com>
2024-03-13 21:33:03 +0300

    add media, change models.py, add ticket in admin

--- main/models.py
+++ main/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-class Tiket(models.Model):
-    title = models.CharField(max_length=255, blank=False)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    count = models.IntegerField(default=0)
+class Ticket(models.Model):
+    title = models.CharField(
+        max_length=255, blank=False, verbose_name='Название спектакля')
+    description = models.TextField(verbose_name='Описание')
+    # цена за один билет
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена за билет')
+    discount_price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, verbose_name='Цена со скидкой')
+    # количество билетов макс в зале
+    count = models.IntegerField(default=0, verbose_name='Количество билетов')
+    # возрастные ограничения, например 18+, 12+
+    age_limit = models.CharField(max_length=3, verbose_name='Возрастное ограничения')
+    
+    class Meta:
+        verbose_name = 'Билет'
+        verbose_name_plural = 'Билеты'
+    
+    def __str__(self):
+        return self.title   
--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from django.http import HttpResponse, HttpRequest
+from .models import Ticket
 
 # def home_view(request: HttpResponse):
 #     return HttpResponse(f'''
 #         <h1 style='color: blue;'>Hello, {request.user.email}!</h1>
 #     ''')
 
-def index_view(request: HttpResponse):
-    return HttpResponse(render(request, 'index.html', {}))
+def index_view(request: HttpRequest):
+    tickets = Ticket.objects.all()
+    
+    return HttpResponse(render(request, 'index.html', {
+        'tickets': tickets
+    }))
